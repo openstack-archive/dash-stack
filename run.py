cd1fe10ea26021ef70606f2c54b547ad45daef63
kelepirci <ozkasgarli@gmail.com>
2016-07-08 23:24:41 +0300

    As template, AdminLTE has added
    I have decided to use AdminLTE for HTML template.
    AdminLTE has prepared by dellintosh on his GitHub repo:
    https://github.com/dellintosh/flask-adminlte

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 #!../venv/bin/python
-from app import app
+# under normal circumstances, this script would not be necessary. the
+# sample_application would have its own setup.py and be properly installed;
+# however since it is not bundled in the sdist package, we need some hacks
+# to make it work
 
+import os
+import sys
 
-app.run(debug=True, host="0.0.0.0", port=8000)
+sys.path.append(os.path.dirname(__name__))
+
+from dash import create_app
+
+# create an app instance
+app = create_app()
+
+app.run(debug=True, host='0.0.0.0', port=8000)
