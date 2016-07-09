b1924f727985e50175cdba42d7e894a1e8cc0706
kelepirci <ozkasgarli@gmail.com>
2016-07-09 13:43:18 +0300

    MySql supoort and db related scripts added

--- dash/__init__.py
+++ dash/__init__.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask
-
 import dateutil.parser
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_adminlte import AdminLTE
 
-
+# main dash-stack app
 dash = Flask(__name__)
+
+# AdminLTE for flask; flask_adminlte
 AdminLTE(dash)
 
+# static configuration file
+dash.config.from_object('config')
+
+# db instance
+db = SQLAlchemy(dash)
     
 from dash import views, models
--- dash/models.py
+++ dash/models.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import dateutil.parser
-from dash import dash
+from dash import dash, db
 
 
-class User(object):
-    """
-    Example User object.  Based loosely off of Flask-Login's User model.
-    """
-    full_name = "Ferhat Ozkasgarli"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    full_name = db.Column(db.String(128), index=True)
     avatar = "/static/img/user2-160x160.jpg"
     created_at = dateutil.parser.parse("November 12, 2012")
+    
+    def __refr__(self):
+        return '<User %r>' % (self.nickname)
