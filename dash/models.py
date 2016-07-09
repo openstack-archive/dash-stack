f6df64ec2ea48e7ca8382875db494b2189a651d1
kelepirci <ozkasgarli@gmail.com>
2016-07-09 19:02:24 +0300

    Auth system

--- dash/models.py
+++ dash/models.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
 
 import dateutil.parser
+from passlib.apps import custom_app_context as pwd_context
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import UserMixin
+
 from dash import dash, db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
     email = db.Column(db.String(128), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     full_name = db.Column(db.String(128), index=True)
     avatar = "/static/img/user2-160x160.jpg"
     created_at = dateutil.parser.parse("November 12, 2012")
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+        
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     
     def __refr__(self):
         return '<User %r>' % (self.nickname)
--- run.py
+++ run.py
@@ -4,4 +4,5 @@
 from dash import dash
 
 # run the dash application
-dash.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    dash.run(debug=True, host='0.0.0.0', port=8000)
