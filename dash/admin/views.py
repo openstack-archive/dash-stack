0b720b9cc0ed5827ad17889cc81af02ceb0b30a1
kelepirci <ozkasgarli@gmail.com>
2016-08-08 21:34:26 +0300

    User Management for Admin
    User management for admin

--- dash/admin/views.py
+++ dash/admin/views.py
@@ -12,8 +12,15 @@ from ..models import User
 from ..email import send_email
 from ..decorators import requires_roles
 
-@admin.route('/index')
+@admin.route('/')
 @login_required
 @requires_roles("admin")
-def for_admins_only():
+def index():
     return render_template('admin/index.html')
+    
+@admin.route('/list-users')
+@login_required
+@requires_roles("admin")
+def list_users():
+    users = User.query.all()
+    return render_template('admin/list_users.html', users=users)
