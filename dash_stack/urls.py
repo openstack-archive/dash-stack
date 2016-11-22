5e2013699b31c6faebab8f9c707d4d4c81cd6a74
Paarhati Ozkasgarli <ozkasgarli@gmail.com>
2016-11-22 20:55:23 +0000

    Template System
    Adding basic template system.
    
    Change-Id: I747a417fe6f821964c9fb708d69acc9538567c4c
    Signed-off-by: Paarhati Ozkasgarli <ozkasgarli@gmail.com>

--- dash_stack/urls.py
+++ dash_stack/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+
+from . import views
 
 urlpatterns = [
     url(r'^admindj/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^auth/', include('authcp.urls')),
 ]
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.TextField(max_length=500, blank=True)
+    provider_password = models.CharField(max_length=50)
+    selected_provider = models.IntegerField()
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
