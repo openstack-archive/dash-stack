3da226dce33a4e8cb6bfb33545480beb8529a619
Paarhati Ozkasgarli <ozkasgarli@gmail.com>
2016-12-04 14:11:25 +0000

    Improvment on Provider
    Some model improvment on provider
    
    Change-Id: Id55e7cd982494554b3391a43a1061e68072248d5
    Signed-off-by: Paarhati Ozkasgarli <ozkasgarli@gmail.com>

--- provider/models.py
+++ provider/models.py
@@ -2,42 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class Provider(models.Model):
-    id = models.AutoField(primary_key=True)
-    provider = models.CharField(max_length=64)
-    name = models.CharField(
-        max_length=255,
-        unique=True,
-        db_index=True
-    )
-    region = models.CharField(
-        max_length=255,
-        unique=True,
-        db_index=True
-    )
-    project_id = models.CharField(
-        max_length=255,
-        db_index=True
-    )
-    default_role = models.CharField(max_length=255)
-    default_domain_id = models.CharField(max_length=255)
-    username = models.CharField(
-        max_length=255,
-        unique=True,
-        db_index=True
-    )
-    password = models.CharField(max_length=255)
-    api_version = models.CharField(max_length=255)
-    url = models.TextField()
-    created_at = models.DateField()
-    enabled = models.BooleanField(default=True)
-    validated = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Type(models.Model):
     types = (
         ('1', 'Public Cloud Provider'),
@@ -49,7 +13,7 @@ class Type(models.Model):
     name = models.CharField(
         max_length=255,
         unique=True,
-        db_index=True
+        db_index=True,
     )
     type = models.CharField(
         max_length=1,
@@ -59,4 +23,36 @@ class Type(models.Model):
 
     def __unicode__(self):
         return  self.name
+
+class Provider(models.Model):
+    id = models.AutoField(primary_key=True)
+    provider = models.ForeignKey(Type)
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+    )
+    region = models.CharField(
+        max_length=255,
+        db_index=True,
+    )
+    project_id = models.CharField(
+        max_length=255,
+        db_index=True,
+    )
+    default_role = models.CharField(max_length=255)
+    default_domain_id = models.CharField(max_length=255)
+    username = models.CharField(
+        max_length=255,
+        db_index=True,
+    )
+    password = models.CharField(max_length=255)
+    api_version = models.CharField(max_length=255)
+    url = models.TextField()
+    created_at = models.DateField()
+    enabled = models.BooleanField(default=True)
+    validated = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
 
