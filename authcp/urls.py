284c955ec6a86ededb0e7374e520d7110bad0462
Paarhati Ozkasgarli <ozkasgarli@gmail.com>
2017-03-11 21:04:58 +0300

    Update settings and Fix User Activation
    Updated some of the values in settings.py and fixed user
    activation email template.
    
    Change-Id: I1eb60b624ad5d6516b7750ddbb90dc0d5087e62a

--- authcp/urls.py
+++ authcp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'authcp/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^register/$', views.register_user, name='register'),
+    url(r'^register-activate/(?P<key>.+)$', views.activation, name='register-activate'),
     url(r'^register-success/$', views.register_success, name='register-success'),
 ]
