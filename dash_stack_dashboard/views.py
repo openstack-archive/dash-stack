335dd99fb91b9c3936a235b614b3afea7b1b2747
Thierry Carrez <thierry@openstack.org>
2019-09-26 16:36:06 +0200

    Retire github mirror, repo moved to opendev

--- dash_stack_dashboard/views.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-@login_required
-def index(request):
-    return render(request, "base.html", {'time' : datetime.datetime.now()})
--- user_profile/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
